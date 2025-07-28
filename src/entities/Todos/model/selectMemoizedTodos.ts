201400530953e4863906f3aaf26911f94edaef26
Danila-A <dan4ik.rus@mail.ru>
2025-07-29 00:14:00 +0300

    had made the 'search' slice at 'entities' layer, but stoped in the 'selectMemoizedTodos.ts'

--- src/entities/Todos/model/selectMemoizedTodos.ts
+++ src/entities/Todos/model/selectMemoizedTodos.ts
@@ -1,10 +1,11 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { selectTodos } from "./todoSlice";
 import { selectFilter } from "../../Filters";
+import { selectSearchValue } from "../../Search";
 
 export const selectMemoizedTodos = createSelector(
-    [selectTodos, selectFilter],
-    (todos, filter) => {
+    [selectTodos, selectFilter, selectSearchValue],
+    (todos, filter, searchValue) => {
         if (filter == 'complete') {
             return todos?.filter((item) => item.status == true);
         }
