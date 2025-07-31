1af0179503105670f774f88e464ab8e59074dac4
Danila-A <dan4ik.rus@mail.ru>
2025-07-31 19:34:19 +0300

    modified the todos memoized selector of rtk by adding the new filtering case - search value

--- src/entities/Todos/model/selectMemoizedTodos.ts
+++ src/entities/Todos/model/selectMemoizedTodos.ts
@@ -2,18 +2,29 @@ import { createSelector } from "@reduxjs/toolkit";
 import { selectTodos } from "./todoSlice";
 import { selectFilter } from "../../Filters";
 import { selectSearchValue } from "../../Search";
+import type { TodoItem } from "./types";
 
 export const selectMemoizedTodos = createSelector(
     [selectTodos, selectFilter, selectSearchValue],
     (todos, filter, searchValue) => {
         if (filter == 'complete') {
-            return todos?.filter((item) => item.status == true);
+            return filterBySearchValue(searchValue, todos)?.filter((item) => item.status == true);
         }
 
         if (filter == 'incomplete') {
-            return todos?.filter((item) => item.status == false);
+            return filterBySearchValue(searchValue, todos)?.filter((item) => item.status == false);
         }
 
-        return todos;
+        return filterBySearchValue(searchValue, todos);
     }
 );
+
+function filterBySearchValue(searchValue: string, todos: TodoItem[] | null): TodoItem[] | null {
+    if (!searchValue) return todos;
+    if (todos) {
+        const filteredBySearch = todos.filter((item) => item.text.includes(searchValue));
+        return filteredBySearch ? filteredBySearch : null;
+    } else {
+        return null;
+    }
+}
