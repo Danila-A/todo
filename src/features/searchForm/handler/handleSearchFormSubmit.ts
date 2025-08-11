9c3d250468ecf8f5449fa123473ef406fa29898a
Nigger <Nigger@coolboy.com>
2025-08-11 15:18:09 +0300

    made searchForm slice of features layer

--- src/features/searchForm/handler/handleSearchFormSubmit.ts
+++ src/features/searchForm/handler/handleSearchFormSubmit.ts
@@ -1,7 +1,9 @@
+import type { SubmitHandler } from "react-hook-form";
 import { useAppDispatch } from "../../../app";
 import { setSearchValue } from "../../../entities/Search";
+import type { SearchFormValue } from "../../../app/types";
 
-export function handleSearchFormSubmit(value: string) {
-    if (!value.trim())  return;
-    useAppDispatch()(setSearchValue({ value }));
+export const handleSearchFormSubmit: SubmitHandler<SearchFormValue> = (data) => {
+    if (!data.search.trim())  return;
+    useAppDispatch()(setSearchValue({ value: data.search }));
 }
--- src/features/searchForm/ui/SearchForm.tsx
+++ src/features/searchForm/ui/SearchForm.tsx
@@ -5,14 +5,16 @@ import { useAppSelector } from '../../../app';
 import { selectMemoizedTheme } from '../../../entities/Themes';
 import { useForm } from 'react-hook-form';
 import type { SearchFormValue } from '../../../app/types';
+import { handleSearchFormSubmit } from '../handler/handleSearchFormSubmit';
 
 export const SearchForm: FC = () => {
     const themeType = useAppSelector(selectMemoizedTheme);
-    const { register } = useForm<SearchFormValue>();
+    const { register, handleSubmit } = useForm<SearchFormValue>();
+    
     return (
-        <div>
-            <form>
-                <div className="">
+        <div className={ styles.wrapper }>
+            <form onSubmit={handleSubmit(handleSearchFormSubmit)}>
+                <div className={ styles.inner }>
                     <Input<SearchFormValue>
                         inputType={ "search" }
                         placeholder={ 'Search note...' }
@@ -20,7 +22,7 @@ export const SearchForm: FC = () => {
                         label={ 'search' }
                         register={ register }
                     />
-                    <div>
+                    <div className={ styles.buttonWrapper }>
                         <button type='submit'>
                             <LensIcon />
                         </button>
