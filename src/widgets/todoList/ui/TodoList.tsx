07d5629f3bdee978d30a6f12a100ea1151d97bf8
Danila-A <dan4ik.rus@mail.ru>
2025-08-13 23:54:50 +0300

    Create todo list widget

---
+++ src/widgets/todoList/ui/TodoList.tsx
@@ -0,0 +1,47 @@
+import type { FC } from "react";
+import type { TodoItem } from "../../../entities/Todos/model/types";
+import { Checkbox } from "../../../shared";
+import { TodoItemText } from "../../../entities/Todos";
+import { TodoDeleteButton } from "../../../features/todoDeleteButton";
+import { deleteTodoHandler } from "../../../features/todoDeleteButton/handler/deleteTodoHandler";
+import { ModalOpenButton } from "../../../features/modalOpenButton";
+import styles from './TodoList.module.scss';
+
+interface Props {
+    todos: TodoItem[];
+}
+
+export const TodoList: FC<Props> = ({ todos }) => {
+    return (
+        <main className={styles.main}>
+            <div className={styles.main__inner}>
+
+                {todos.map((item) => (
+                    <div key={item.id} className={styles.listItem}>
+                        <div className={styles.listItem__inner}>
+
+                            <div className={styles.listItem__checkbox}>
+                                <Checkbox 
+                                    clickHandler={deleteTodoHandler} 
+                                    checked={item.status}
+                                    id={item.id}
+                                />
+                            </div>
+                            <div>
+                                <TodoItemText text={item.text} />    
+                            </div>
+                            <div className={styles.listItem__deleteButton}>
+                                <TodoDeleteButton id={item.id} />
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+
+                <div className={styles.main__modalOpenButton}>
+                    <ModalOpenButton />
+                </div>
+            </div>
+        </main>
+    );
+}
