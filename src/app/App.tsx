0e5420d23a90c0d2bda3ba42eafdc8904901ef10
Danila-A <dan4ik.rus@mail.ru>
2025-08-21 19:55:02 +0300

    Add 'Main' pages folder into pages layer
    I forgot to do it and segments of main page were directly
    into pages layer.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import './Styles/Styles.scss';
 import '../shared/fonts/Styles/Inter.scss';
 import { MainProvider } from './provider/MainProvider';
-import { Main } from '../pages';
+import { Main } from '../pages/Main';
 
 export const App = () => {
   return (
