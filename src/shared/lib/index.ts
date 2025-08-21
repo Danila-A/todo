5acf83e50366efc982a370b88773a5a2cd1935ee
Danila-A <dan4ik.rus@mail.ru>
2025-08-21 21:01:13 +0300

    Replace a hoc into a place where it only use
    Before 'WithImagePlaceholder' hoc was at shared layer.
    But he only was used one place.
    
    I replace the hoc there so at todoList slice of widgets layer.

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,4 +1,3 @@
 export { useAppDispatch, useAppSelector } from './store/customReduxHooks.ts';
 export { setDataToLocalStorage } from './localStorage/setDataToLocalStorage.ts';
 export { getDataFromLocalStorage } from './localStorage/getDataFromLocalStorage.ts';
-export { WithImagePlaceholder } from './hocs/WithImagePlaceholder.ts';
--- src/widgets/todoList/lib/WithImagePlaceholder.tsx
+++ src/widgets/todoList/lib/WithImagePlaceholder.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 import { EmptyList } from "../../../widgets/emptyList";
-import { selectMemoizedTodos } from "../../../app";
-import { useAppSelector } from "../store/customReduxHooks";
+import { selectMemoizedTodos } from "../../../features/searchTodo";
+import { useAppSelector } from "../../../shared/lib";
 
 export const WithImagePlaceholder = (Component: FC) => {
     const todos = useAppSelector(selectMemoizedTodos);
