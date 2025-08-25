935974bee25da9b4901c1664f9b430eb717a517d
Danila-A <dan4ik.rus@mail.ru>
2025-08-25 22:02:58 +0300

    WIP: Remove unneeded custom hook

--- src/pages/Main/lib/useRetrieveLocalStorage.tsx
+++
@@ -1,9 +0,0 @@
-import { retrieveThemeTypeFromLocalStorage } from "../../../entities/Theme";
-import { retrieveTodosFromLocalStorage } from "../../../entities/Todo";
-import { useAppDispatch } from "../../../shared/lib"
-
-export const useRetrieveLocalStorage = () => {
-    const dispatch = useAppDispatch();
-    dispatch(retrieveThemeTypeFromLocalStorage());
-    dispatch(retrieveTodosFromLocalStorage());
-}
