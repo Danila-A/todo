2059f48c747a8a0288afa1cd727c149f413bd7dc
Danila-A <dan4ik.rus@mail.ru>
2025-09-09 23:10:23 +0300

    Fix filters button behavior
    When user clicked outside the filters button when it was open it didn't
    close.
    
    I added handler that close filters button when user clicks outside it.

---
+++ src/features/changeFilter/lib/useFilterButton.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export const useFilterButton = (
+    isVisible: boolean, 
+    setIsVisible: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+    useEffect(() => {
+        const filterButton = document.querySelector('#filterButton') as HTMLDivElement;
+
+        const clickHandler = (event: MouseEvent) => {
+            const eventTarget = event.target as HTMLDivElement;
+            
+            if (isVisible) {
+                if (eventTarget?.closest('#filterButton') !== filterButton) {
+                    setIsVisible(false);
+                }
+            }
+        }
+
+        document.addEventListener('click', clickHandler);
+
+        return () => {
+            document.removeEventListener('click', clickHandler);
+        }
+    }, [isVisible]);
+}
